90f87b4b6f70f9a9979f2f1caae15f5279a0ef29
Carlos Aguilar Miguel <nukiro@yahoo.com>
2025-07-06 17:09:28 +0200

    feat: Basic logging

---
+++ logging.h
@@ -0,0 +1,17 @@
+struct Logger;
+
+#define DEBUG 0
+#define INFO 1
+#define WARN 2
+#define ERROR 3
+#define FATAL 4
+#define OFF 5
+
+struct Logger *create_logger(int level);
+void destroy_logger(struct Logger *l);
+
+int debug(const struct Logger *l, const char *msg);
+int info(const struct Logger *l, const char *msg);
+int warn(const struct Logger *l, const char *msg);
+int error(const struct Logger *l, const char *msg);
+int fatal(const struct Logger *l, const char *msg);
