38be7e9ecf033cb7e9084895da468f1b5d0ab9d1
Carlos Aguilar Miguel <164380566+nukiro@users.noreply.github.com>
2025-07-21 12:15:01 +0200

    feat: detect os (#53)
    Using C preprocessor macros we detect OS. This is the first step on game
    setup.
    
    Remove FPS structure.
    
    Closes #51

---
+++ game.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "logging.h"
+
+enum Environment
+{
+    DEVELOPMENT,
+    PRODUCTION,
+};
+
+enum OS
+{
+    WINDOWS,
+    MACOS,
+    LINUX,
+    UNIX,
+};
+
+struct Game
+{
+    char version[50];
+    char name[50];
+    struct
+    {
+        int width;
+        int heigth;
+    } window;
+    bool debug;
+    enum Environment environment;
+    enum OS os;
+    int target_fps;
+};
+
+struct Game create_game();
+
+const char *environment_game(const struct Game *game);
+// const char *window_game(const struct Game *game)
+const char *window_game();
+
+bool setup_game(struct Game *game, const struct Logger *logger);
