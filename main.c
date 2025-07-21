5178557e651e77f144a76501ee2a3e962d301f8c
Carlos Aguilar Miguel <164380566+nukiro@users.noreply.github.com>
2025-07-21 12:15:01 +0200

    feat: detect os (#53)
    Using C preprocessor macros we detect OS. This is the first step on game
    setup.
    
    Remove FPS structure.
    
    Closes #51

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "logging.h"
 #include "camera.h"
 #include "hero.h"
+#include "game.h"
 
 #include <raylib.h>
 #include <raymath.h>
@@ -18,55 +19,6 @@ struct Player
 int trace(const struct Player *player)
 {
     return player->traceable ? INFO : ERROR;
-}
-
-struct FPS
-{
-    int target;
-    double previous_time;    // Previous time measure
-    double current_time;     // Current time measure
-    double update_draw_time; // Update + Draw time
-    double wait_time;        // Wait time (if target fps required)
-    float delta_time;        // Frame time (Update + Draw + Wait time)
-};
-
-struct Game
-{
-    char version[50];
-    char name[50];
-    struct
-    {
-        int width;
-        int heigth;
-    } window;
-    bool debug;
-    struct FPS fps;
-};
-
-struct Scene
-{
-    Color background;
-};
-
-void calculate_fps(struct FPS *p)
-{
-    p->current_time = GetTime();
-    p->update_draw_time = p->current_time - p->previous_time;
-
-    if (p->target > 0) // We want a fixed frame rate
-    {
-        p->wait_time = (1.0f / (float)p->target) - p->update_draw_time;
-        if (p->wait_time > 0.0)
-        {
-            WaitTime((float)p->wait_time);
-            p->current_time = GetTime();
-            p->delta_time = (float)(p->current_time - p->previous_time);
-        }
-    }
-    else
-        p->delta_time = (float)p->update_draw_time; // Framerate could be variable
-
-    p->previous_time = p->current_time;
 }
 
 bool double_click(bool *first_click, float *last_click_time)
@@ -97,40 +49,31 @@ static bool first_click = false;
 int main(void)
 {
     /* Initialization */
-
     struct Player player = {"UUID_PLAYER", true};
-    struct Game game =
-        {
-            .version = "v0.1.0",
-            .name = "Queen of Shadows",
-            .window = {1920, 1080},
-            .debug = true,
-            .fps = {
-                .target = 60,
-                .previous_time = GetTime(),
-                .current_time = 0.0,
-                .update_draw_time = 0.0,
-                .wait_time = 0.0,
-                .delta_time = 0.0f}};
-
+    struct Game game = create_game();
     struct Logger logger = create_logger(game.debug ? DEBUG : trace(&player));
-    info(&logger, "Initializating Game...");
+
+    info(&logger, "Initializating...");
+
+    // if setup fails return error
+    if (!setup_game(&game, &logger))
+    {
+        return 0;
+    }
+
+    info(&logger, "Running...");
 
     InitWindow(game.window.width, game.window.heigth, game.name);
-    SetTargetFPS(game.fps.target);
+    SetTargetFPS(game.target_fps);
 
     struct Hero hero = create_hero((Vector3){.0f, .0f, 0.f});
-    // initialize camema by hero position
+    // initialize camera by hero position
     struct Camera camera = create_camera(hero.position);
 
     while (!WindowShouldClose())
     {
         // float _ = GetFrameTime();
         // printf("%f %f\n", hero.position.x, hero.position.z);
-
-        // Update
-        if (game.debug)
-            calculate_fps(&game.fps);
 
         // Action Input
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
@@ -170,9 +113,8 @@ int main(void)
         if (game.debug)
         {
             DrawText(TextFormat("%s %s", game.name, game.version), 10, 10, 10, GREEN);
-            DrawText(TextFormat("FPS Current: %i (target: %i)", (int)(1.0f / game.fps.delta_time), game.fps.target), 10, 30, 10, GREEN);
-            DrawText(TextFormat("Camara: %s (%.0f)", position_camera(&camera), camera.angle), 10, 45, 10, GREEN);
-            DrawText(TextFormat("Hero: %.2f %.2f", hero.position.x, hero.position.z), 10, 60, 10, GREEN);
+            DrawText(TextFormat("%s (%.0f)", position_camera(&camera), camera.angle), 10, 30, 10, GREEN);
+            DrawText(TextFormat("Hero: %.2f %.2f", hero.position.x, hero.position.z), 10, 45, 10, GREEN);
         }
         EndDrawing();
     }
