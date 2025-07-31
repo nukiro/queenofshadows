a8e23202d8f0a3ce171d08f7735651658614189a
Carlos Aguilar Miguel <nukiro@yahoo.com>
2025-07-31 09:03:30 +0200

    showing help info

--- zmake/README.md
+++ zmake/README.md
@@ -1,10 +1,31 @@
-# bar - C Project Builder
+# `zmake` - C Project Builder written in Zig
 
 A command-line tool written in Zig to build and run C programs.
 
 ## Installation
 
-```bash
-zig build
-sudo zig build install
+```shell
+
+```
+
+## Development
+
+After build the program `zig build`, you can run it:
+
+Running the executable program
+
+```shell
+# Within zig-out project directory
+./zmake --help
+```
+
+Create a symlink and run the command
+
+```shell
+# From your zig-out project directory
+sudo ln -sf $(pwd)/zmake /usr/local/bin/zmake
+# check it
+which zmake
+# then
+zmake --help
 ```
--- zmake/src/main.zig
+++ zmake/src/main.zig
@@ -1,1 +1,40 @@
-pub fn main() !void {}
+const std = @import("std");
+const print = std.debug.print;
+
+fn printUsage() void {
+    print("zmake - C Project Builder\n\n", .{});
+    print("Usage: zmake [OPTIONS]\n\n", .{});
+    print("Options:\n", .{});
+    print("  --folder <path>     Specify the project folder (required)\n", .{});
+    print("  -o <key>=<value>    Set build options (debug=true/false, output=name)\n", .{});
+    print("  --compiler <name>   Specify compiler (default: gcc)\n", .{});
+    print("  --no-run           Don't run the program after building\n", .{});
+    print("  --verbose          Enable verbose output\n", .{});
+    print("  --help             Show this help message\n\n", .{});
+    print("Examples:\n", .{});
+    print("  zmake --folder playground/camera -o debug=true\n", .{});
+    print("  zmake --folder myproject -o output=myapp -o debug=false\n", .{});
+    print("  zmake --folder src --compiler clang --verbose\n", .{});
+}
+
+fn parseArgs() !void {
+    var args = std.process.args();
+    _ = args.skip(); // Skip program name
+
+    while (args.next()) |arg| {
+        if (std.mem.eql(u8, arg, "--help") or std.mem.eql(u8, arg, "-h")) {
+            printUsage();
+            std.process.exit(0);
+        } else {
+            print("Error: Unknown argument '{s}'\n", .{arg});
+            print("Use --help for usage information\n", .{});
+            return error.InvalidArgument;
+        }
+    }
+}
+
+pub fn main() !void {
+    parseArgs() catch {
+        std.process.exit(1);
+    };
+}
